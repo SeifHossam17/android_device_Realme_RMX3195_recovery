cabb6183199c30be4fa60fd39d4c9d22030c8ce5
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-01-17 22:40:45 +0000

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_RMX3195.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_RMX3195-user \
+    omni_RMX3195-userdebug \
+    omni_RMX3195-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for realme RMX3195 (RMX3195)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
