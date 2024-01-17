e18aabe54e81844197988f64bc05f413fb6d7546
Kareem Ebrahem <safen894@gmail.com>
2024-01-18 01:47:29 +0200

    RMX3195: omni --> twrp

--- twrp_RMX3195.mk
+++ twrp_RMX3195.mk
@@ -8,15 +8,16 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RMX3195 device
 $(call inherit-product, device/realme/RMX3195/device.mk)
 
 PRODUCT_DEVICE := RMX3195
-PRODUCT_NAME := omni_RMX3195
+PRODUCT_NAME := twrp_RMX3195
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3195
 PRODUCT_MANUFACTURER := realme
@@ -25,5 +26,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 13 TP1A.220905.001 1691489594194 release-keys"
-
-BUILD_FINGERPRINT := None
